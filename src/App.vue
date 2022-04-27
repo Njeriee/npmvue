521f1936cb63b948bd6e4ff3deead4efc250c4de
Njeriee <savelinnjeri@gmail.com>
2022-04-27 18:42:01 +0300

    vue js as a dependancy instead of cdn

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'sav'
+    }
+  },
+}
+</script>
+
+<template>
+  <p class = 'hey'>hello {{ name }} welcome to Vue</p>
+</template>
+
+<style>
+.hey{
+  justify-content: center;
+    position: absolute;
+    top: 500px;
+    left: 40%;
+    font-size: 50px;
+    font-family: Arial, Helvetica, sans-serif;
+    background-color: yellowgreen;
+}
+</style>
---
+++ src/components/firstVue.vue
@@ -0,0 +1,16 @@
+<script setup>
+// js code will go here
+</script>
+
+<template>
+    <h1>Hello{{name}}</h1>
+</template>
+
+<style scoped>
+h1{
+    color:red;
+    font-family: sans-serif;
+    font-family: sans-serif;
+    font-size: 100px;
+}
+</style>
